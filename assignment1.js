8b5c0dbab2487b0bc149b31dc6a7480d26f02106
Ankita2059 <ankitakhatrichhetri@gmail.com>
2023-07-03 22:31:55 +0545

    initial addition

---
+++ assignment1.js
@@ -0,0 +1,16 @@
+function positive (num) {
+    if(num>0) {
+    return "num is positive number" ;
+    }
+    else if (num<0) {
+      return "num is negative number";
+    }
+      else if (num==0){
+          return "Given number is zero";
+      }
+  }
+console.log(positive(9))
+console.log(positive(-7))
+console.log(positive(0))
+
+
