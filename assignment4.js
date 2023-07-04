51b53e3e719c91da3242c67f52893d444b7e63be
Ankita2059 <ankitakhatrichhetri@gmail.com>
2023-07-04 09:07:57 +0545

    changes

---
+++ assignment4.js
@@ -0,0 +1,6 @@
+var temp ='hi';//variable declaration
+ function display(){  // display is function name and it doesn't take any parameters.
+    console.log(temp); // it will log 'hi' since 'temp' was assigned that value.
+    var temp ='bye'; // temp variable mah bye value assigned
+ };
+ display(); // logs the value of the temp and prints undefined.
